e5edf8ab553abdef5d4d665d6108e4586d068c6d
shahid <shahidyamdi.com>
2024-05-03 11:13:21 +0530

    validation

--- Microservice/springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
+++ Microservice/springboot-restful-webservices/src/main/java/net/javaguides/springboot/controller/UserController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import net.javaguides.springboot.dto.UserDto;
 import net.javaguides.springboot.service.UserService;
@@ -22,45 +23,44 @@ import net.javaguides.springboot.service.UserService;
 @RequestMapping("api/users")
 public class UserController {
 
-    private UserService userService;
+	private UserService userService;
 
-    // build create User REST API
-    @PostMapping
-    public ResponseEntity<UserDto> createUser(@RequestBody UserDto user){
-        UserDto savedUser = userService.createUser(user);
-        return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
-    }
+	// build create User REST API
+	@PostMapping
+	public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto user) {
+		UserDto savedUser = userService.createUser(user);
+		return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
+	}
 
-    // build get user by id REST API
-    // http://localhost:8080/api/users/1
-    @GetMapping("{id}")
-    public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId){
-        UserDto user = userService.getUserById(userId);
-        return new ResponseEntity<>(user, HttpStatus.OK);
-    }
+	// build get user by id REST API
+	// http://localhost:8080/api/users/1
+	@GetMapping("{id}")
+	public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long userId) {
+		UserDto user = userService.getUserById(userId);
+		return new ResponseEntity<>(user, HttpStatus.OK);
+	}
 
-    // Build Get All Users REST API
-    // http://localhost:8080/api/users
-    @GetMapping
-    public ResponseEntity<List<UserDto>> getAllUsers(){
-        List<UserDto> users = userService.getAllUsers();
-        return new ResponseEntity<>(users, HttpStatus.OK);
-    }
+	// Build Get All Users REST API
+	// http://localhost:8080/api/users
+	@GetMapping
+	public ResponseEntity<List<UserDto>> getAllUsers() {
+		List<UserDto> users = userService.getAllUsers();
+		return new ResponseEntity<>(users, HttpStatus.OK);
+	}
 
-    // Build Update User REST API
-    @PutMapping("{id}")
-    // http://localhost:8080/api/users/1
-    public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId,
-                                           @RequestBody UserDto user){
-        user.setId(userId);
-        UserDto updatedUser = userService.updateUser(user);
-        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
-    }
+	// Build Update User REST API
+	@PutMapping("{id}")
+	// http://localhost:8080/api/users/1
+	public ResponseEntity<UserDto> updateUser(@Valid @PathVariable("id") Long userId, @RequestBody UserDto user) {
+		user.setId(userId);
+		UserDto updatedUser = userService.updateUser(user);
+		return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+	}
 
-    // Build Delete User REST API
-    @DeleteMapping("{id}")
-    public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId){
-        userService.deleteUser(userId);
-        return new ResponseEntity<>("User successfully deleted!", HttpStatus.OK);
-    }
+	// Build Delete User REST API
+	@DeleteMapping("{id}")
+	public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId) {
+		userService.deleteUser(userId);
+		return new ResponseEntity<>("User successfully deleted!", HttpStatus.OK);
+	}
 }
